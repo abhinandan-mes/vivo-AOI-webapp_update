78b4f6419b41c62b66b4c285866cfaf6b2942d4c
abhinandan <abhinandan@example.com>
2026-09-14 18:58:19 +0530

    fix: Switch to HashRouter to resolve IIS 500/404 routing errors without web.config dependencies

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+﻿import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { LanguageProvider } from './contexts/LanguageContext';
@@ -8,10 +8,10 @@ import { LanguageProvider } from './contexts/LanguageContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <LanguageProvider>
         <App />
       </LanguageProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
